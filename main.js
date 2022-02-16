dacf4dd26870a041accef9bbd18e5525a560f65f
GEAN PEREIRA DE SOUZA <21276338@CFP127ALUNO.LOCAL>
2022-02-16 16:22:11 -0300

    completo

---
+++ main.js
@@ -0,0 +1,103 @@
+'use strict'
+
+const semaforo = document.getElementById('semaforo')
+const verde = document.getElementById('verde')
+const amarelo = document.getElementById('amarelo')
+const vermelho = document.getElementById('vermelho')
+const automatico = document.getElementById('automatico')
+let idAutomatico = null;
+
+
+////////////////////////////////////////////////////////////////
+const cores = {
+    verde: "img/verde.png",
+    vermelho: "img/vermelho.png",
+    amarelo: "img/amarelo.png",
+
+}
+///////////////////////////////////////////////////////////////////
+
+let interval; // id do intervalo usado no modo automatico
+let proximaCor = cores.vermelho;
+
+///////////////////////////////////////////////////////////////////
+
+const semaforoVerde = () => {
+    semaforo.src= "img/verde.png"
+}
+////////////////////////////////////////////////////////////////////
+
+const semaforoAmarelo = () => {
+    semaforo.src= "img/amarelo.png"
+}
+
+
+////////////////////////////////////////////////////////////////////
+
+const semaforoVermelho = () => {
+    semaforo.src= "img/vermelho.png"
+}
+
+
+/////////////////////////////////////////////////////////////////////
+    
+
+const getProximaCor = () => {
+    let proximaCor;
+
+    if (semaforo.src.includes("img/verde.png")) {
+        proximaCor = cores.amarelo;
+    } else if (semaforo.src.includes("img/vermelho.png")) {
+        proximaCor = cores.verde;
+    } else if (semaforo.src.includes("img/amarelo.png")) {
+        proximaCor = cores.vermelho;
+    } else if (semaforo.src.includes("/mg/desligado.png")){
+        proximaCor = cores.verde;
+    }
+
+    return proximaCor;
+}
+
+///////////////////////////////////////////////////////////////////////
+
+const ligarAutomatico = () =>{
+    if (interval == null) {
+        interval = setInterval(() => trocarCor(proximaCor), 1000);
+        automatico.textContent = "Parar";
+    } else {
+        desligarAutomatico()
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+const desligarAutomatico = () => {
+    clearInterval(interval);
+    automatico.textContent = "Automático";
+    interval = null;
+}
+////////////////////////////////////////////////////////////////////////
+
+const clickButtonVerde = () =>{
+    trocarCor(cores.verde);
+    desligarAutomatico();
+}
+
+const clickButtonVermelho = () =>{
+    trocarCor(cores.vermelho);
+    desligarAutomatico();
+}
+
+const clickButtonAmarelo = () =>{
+    trocarCor(cores.amarelo);
+    desligarAutomatico();
+}
+//////////////////////////////////////////////////////////////////////
+const trocarCor = (cor) => {
+    semaforo.src = cor
+    proximaCor = getProximaCor();
+}
+
+verde.addEventListener("click", semaforoVerde)
+amarelo.addEventListener("click", semaforoAmarelo)
+vermelho.addEventListener("click", semaforoVermelho)
+automatico.addEventListener("click", ligarAutomatico)
